8920ddd1a582c7342753090ff13a1a2a2b431eb7
Henrik Haugerud Carlsen <henrikhhc@gmail.com>
2019-11-21 17:56:05 +0100

    updater readfile

--- code/readproject5.py
+++ code/readproject5.py
@@ -13,25 +13,27 @@ def readfile(file):
 
 
     o = open(file)
-    readline()
-    for line in o:
-        line = line.split()
+    n = int(o.readline())
+    lines = o.readline()
+    line = lines.split()
 
-        u.append(float(line[0]))
+    for word in line:
+        u.append(float(word))
 
     o.close()
-    return u
+    return u, n
 
-u = readfile(sys.argv[1])
+u, n = readfile(sys.argv[1])
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='Computer Modern', size=15)
+x = np.linspace(0,1, n+1)
 
 plt.plot(x, u)
 plt.xlabel("position (x)")
-plt.ylabel("Displacement (y)")
+plt.ylabel("Displacement (u)")
 plt.title("Displacement as function of position")
 plt.grid()
 plt.tight_layout()
-plt.savefig('plots/x_Disp.pdf')
+plt.savefig('x_Disp.pdf')
 plt.close()
