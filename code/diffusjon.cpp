f78b363ef2026f847aaa63cd80df01f869448b2d
unknown <oyvindaf>
2019-11-25 17:45:27 +0100

    update on backward

--- code/diffusjon.cpp
+++ code/diffusjon.cpp
@@ -16,18 +16,16 @@ struct Variables{
 	double *L;
 	double *U;
 	double *y;
-	double *f;
 	double *u;
 	double *u_new;
 	int length;
-	int a;
-	int b;
-	int c;
-	int n;
 	int tsteps;
 	double dx;
 	double dt;
 	double alpha;
+	int a;
+	int b;
+	int c;
 };
 
 void LU_decomp(Variables& var){
@@ -38,33 +36,46 @@ void LU_decomp(Variables& var){
 
 		var.L[k] = var.a/var.U[k-1];
 		var.U[k] = var.b - var.L[k]*var.c;
+		// cout << var.L[k] << endl;
+		// cout << var.U[k] << endl;
 	}
 }
 
-void solve_Ly_f(Variables& var){
-	var.y[0] = var.f[0];
-
+void solve_Ly_u(Variables& var){
+	var.y[0] = var.u[0];
+	// cout << var.u_new[0] << endl;
+	// cout << var.y[0] << endl;
 	for (int k=1; k< var.length; k++){
-		var.y[k] = var.f[k] - var.L[k]*var.y[k-1];
+		var.y[k] = var.u[k] - var.L[k]*var.y[k-1];
+		// cout << var.y[k] << endl;
 	}
 
 }
 
-void solve_Uu_f(Variables& var){
-	var.u[var.length-1] = var.y[var.length-1]/var.U[var.length-1];
-
+void solve_UuNew_u(Variables& var){
+	var.u_new[var.length-1] = var.y[var.length-1]/var.U[var.length-1];
+	// cout << var.u_new[var.length-1];
 	for (int k = var.length-2; k != -1; k--){
-		var.u[k] = var.y[k]/var.U[k] - var.c * var.u[k+1] / var.U[k];
+		var.u_new[k] = var.y[k]/var.U[k] - var.c * var.u_new[k+1] / var.U[k];
+		// cout << var.u_new[k] << endl;
 	}
+}
+
+void thomas_algorithm(Variables& var){
+	LU_decomp(var);
+	solve_Ly_u(var);
+	solve_UuNew_u(var);
 }
 
 void Forward_Euler(int n, int tsteps, double dx, double dt, double alpha) {
-	double *u, *u_new;
+	double *u;
+	double *u_new;
 	u = new double[n+1]; u_new = new double[n + 1];
 
 	u[n] = 1; u_new[n] = 0; //boundary conditions and initial conidtions
 	for (int i = 0; i < n; i++) {
-		u[i] = 0; u_new[i] = 0;
+		u[i] = 0;
+		u_new[i] = 0;
 	}
 	for (int t = 1; t <= tsteps; t++) { //Forward Euler algorithm
 		for (int i = 1; i < n; i++) {
@@ -79,29 +90,27 @@ void Forward_Euler(int n, int tsteps, double dx, double dt, double alpha) {
 }
 
 void Backward_Euler(Variables& var){
-	var.u[var.length-1]=1; // boundary conditions and initial conditions
-	var.u_new[var.length-1]=0;
-	for(int i=1; i<var.length; i++){
+	var.u[var.length]=1; // boundary conditions and initial conditions
+
+	for(int i=0; i<var.length; i++){
 		var.u[i]=0;
 		var.u_new[i]=0;
 	}
 	for(int t=1; t<var.tsteps; t++){
 		LU_decomp(var);
-		solve_Ly_f(var);
-		solve_Uu_f(var);
-		var.u[0]=0;
-		var.u[var.length-1]=1; //boundary conditions again
-		for(int i=0; i<var.length; i++){
-			var.y[i]=var.u[i];
+		solve_Ly_u(var);
+		solve_UuNew_u(var);
+		for(int i=1; i<var.length; i++){
+			var.u[i]=var.u_new[i];
 		}
 	}
-	// Output(n, u, tsteps, dt);
+	Output(var.length, var.u, var.tsteps, var.dt);
 }
 
-void Crank_Nicholsen(Variables& var){
-	var.u[var.length-1]=1;
-	var.u_new[var.length-1]=0
-}
+// void Crank_Nicholsen(Variables& var){
+// 	var.u[var.length-1]=1;
+// 	var.u_new[var.length-1]=0
+// }
 
 void Output(int n, double* u, int tsteps, double dt) {
 	//function for writing the results in a file
@@ -130,21 +139,28 @@ int main(int argc, char* argv[]) {
 	int n, tsteps;
 	double dx, dt, alpha;
 
+	read_input(tsteps, dx, dt);
+
 	Variables sol;
-	sol.length = 3;
-	sol.L = new double[sol.length];
-	sol.U = new double[sol.length];
-	sol.y = new double[sol.length];
-	sol.f = new double[sol.length];
-	sol.u = new double[sol.length];
-	sol.u_new = new double[sol.length];
-	sol.y = new double[sol.length];
-	sol.a=1;
-	sol.b=2;
-	sol.c=1;
-	for (int i=0; i<sol.length; i++){
-		sol.f[i] = 2;
-		}
+	sol.dt = dt;
+	sol.dx = dx;
+	sol.tsteps = tsteps;
+	sol.alpha = sol.dt/sol.dx/sol.dx;
+	sol.length = (int)(1/sol.dx);
+	sol.L = new double[sol.length+1];
+	sol.U = new double[sol.length+1];
+	sol.y = new double[sol.length+1];
+	sol.u = new double[sol.length+1];
+	sol.u_new = new double[sol.length+1];
+
+	sol.a = -sol.alpha;
+	sol.c = -sol.alpha;
+	sol.b = 1 + 2*sol.alpha;
+
+
+
+
+
 
 	if (argc <= 1) {
 		cout << "Bad Usage: " << argv[0] << " read also output file on same line" << endl;
@@ -156,17 +172,6 @@ int main(int argc, char* argv[]) {
 
 	ofile.open(outfilename);
 
-	// read_input(tsteps, dx, dt);
-	n = 1 / dx;
-	alpha = dt / dx / dx;
-
-
-
-
-	LU_decomp(sol);
-	solve_Ly_f(sol);
-	solve_Uu_f(sol);
-
 	Backward_Euler(sol);
-	// Forward_Euler(n, tsteps, dx, dt, alpha);
+	Forward_Euler(n, tsteps, dx, dt, alpha);
 }
